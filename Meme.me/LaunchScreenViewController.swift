e3e9a6c71a023e618e2abee4fa9742760bddcc14
Mariano Zorrilla <mkiisoft@gmail.com>
2015-11-01 07:15:12 -0300

    first version

--- Meme.me/LaunchScreenViewController.swift
+++ Meme.me/LaunchScreenViewController.swift
@@ -19,7 +19,7 @@ class LaunchScreenViewController: UIViewController {
         lauchLogo.transform = CGAffineTransformMakeScale(0.0, 0.0)
         launchSmall.transform = CGAffineTransformMakeScale(0.0, 0.0)
  
-        lauchScreenAnimation()
+        lauchScreenAnimation([lauchLogo, launchSmall], delay: [0, 0.3])
         
     }
     
@@ -27,57 +27,37 @@ class LaunchScreenViewController: UIViewController {
         if segue.identifier == "mainscreen" {}
     }
     
-    func lauchScreenAnimation() {
-        
-        UIView.animateWithDuration(0.8, delay: 0, options: [.CurveEaseInOut], animations: {
-            self.lauchLogo.transform = CGAffineTransformMakeScale(1.2, 1.2)
-            
-            UIView.animateWithDuration(0, delay: 0.2, options: [], animations: {
-                self.launchSmall.transform = CGAffineTransformMakeScale(1.1, 1.1)
+    func lauchScreenAnimation(image: [UIImageView], delay: [NSTimeInterval]) {
+        for i in 0...image.count - 1 {
+            UIView.animateWithDuration(1.2,
+                delay: delay[i],
+                usingSpringWithDamping: 0.3,
+                initialSpringVelocity: 10.0,
+                options: .CurveLinear,
+                animations: {
+                    image[i].transform = CGAffineTransformMakeScale(1, 1)
+                    self.view.layoutIfNeeded()
                 }, completion: nil)
-            
+        }
+        
+        UIView.animateWithDuration(0.3, delay: 2.0, options: [], animations: {
+            self.launchSmall.transform = CGAffineTransformMakeRotation(75)
             }, completion: {
                 (Bool) -> Void in
-                
-                UIView.animateWithDuration(0.3, delay: 0, options: [.CurveEaseInOut], animations: {
-                    self.lauchLogo.transform = CGAffineTransformMakeScale(0.8, 0.8)
-                    UIView.animateWithDuration(0, delay: 0.3, options: [], animations: {
-                        self.launchSmall.transform = CGAffineTransformMakeScale(0.8, 0.8)
-                        }, completion: nil)
-                    
+                UIView.animateWithDuration(0.4, animations: {
+                    self.launchSmall.alpha = 0
                     }, completion: {
                         (Bool) -> Void in
-                        
-                        UIView.animateWithDuration(0.3, delay: 0, options: [.CurveEaseInOut], animations: {
-                            self.lauchLogo.transform = CGAffineTransformMakeScale(1, 1)
-                            UIView.animateWithDuration(0, delay: 0.3, options: [], animations: {
-                                self.launchSmall.transform = CGAffineTransformMakeScale(1, 1)
-                                }, completion: {
-                                    (Bool) -> Void in
-                                    UIView.animateWithDuration(0.3, animations: {
-                                        self.launchSmall.transform = CGAffineTransformMakeRotation(75)
-                                        UIView.animateWithDuration(0.4, delay: 0.8, options: [], animations: {
-                                                self.launchSmall.alpha = 0
-                                            }, completion: nil)
-                                    })
-                            })
+                        UIView.animateWithDuration(0.5, animations: {
+                            self.lauchLogo.transform = CGAffineTransformMakeScale(150, 150)
                             }, completion: {
                                 (Bool) -> Void in
-                                
-                                UIView.animateWithDuration(0.5, delay: 1.5, options: [], animations: {
-                                    
-                                    self.lauchLogo.transform = CGAffineTransformMakeScale(150, 150)
-                                    
-                                    }, completion: {
-                                        (Bool) -> Void in
-                                        self.performSegueWithIdentifier("versionselect", sender: self)
-                                })
-                                
+                                self.performSegueWithIdentifier("versionselect", sender: self)
                         })
                         
                 })
-            }
-        )
+                
+        })
     }
     
 }
